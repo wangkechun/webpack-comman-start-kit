bdc258d11f3aa9622cc5d48942a5fdcb97e13858
wangkechun <hi@hi-hi.cn>
2016-06-08 14:39:02 +0800

    11

--- hello-redux/components/Root.jsx
+++ hello-redux/components/Root.jsx
@@ -5,18 +5,11 @@ import {
   add
 } from '../actions'
 
-const Root = React.createClass({
-	render(){
-		const p = this.props
-		return (
-			<div>
-				<h1>{p.cnt}</h1>
-				<button onClick={()=>p.add()} >add</button>
-			</div>
-		)
-	}
-})
-
+const Root = (p)=>
+	<div>
+		<h1>{p.cnt}</h1>
+		<button onClick={()=>p.add()} >add</button>
+	</div>
 
 export default connect((state, ownProps) => {
   return {
